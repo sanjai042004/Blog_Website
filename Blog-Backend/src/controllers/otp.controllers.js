4bccfa92ffeff1c96d525023755a768386c408a9
sanjai042004 <sanjaiv642@gmail.com>
2025-11-13 18:22:14 +0530

    .

--- Blog-Backend/src/controllers/otp.controllers.js
+++ Blog-Backend/src/controllers/otp.controllers.js
@@ -3,12 +3,15 @@ const nodemailer = require("nodemailer");
 const bcrypt = require("bcrypt");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
+
 
 //Helper Send OTP email
 const sendOtpEmail = async (email, subject, otp) => {
