67fc2cf252dd40e334a2440b96036633f3637a13
sanjai042004 <sanjaiv642@gmail.com>
2026-01-25 17:00:37 +0530

    .

--- Blog-Backend/server.js
+++ Blog-Backend/server.js
@@ -18,7 +18,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://codeverseb.netlify.app",
+      "https://codeverseb.vercel.app",
     ],
     credentials: true,
   })
