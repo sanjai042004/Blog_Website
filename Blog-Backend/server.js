b393ef671f8e8ea5c0807ce9f47b7dae66d4faf0
sanjai042004 <sanjaiv642@gmail.com>
2026-03-13 09:30:42 +0530

    fixing cors issuse

--- Blog-Backend/server.js
+++ Blog-Backend/server.js
@@ -18,7 +18,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://codeverseb.vercel.app",
+      "https://codeverse-c.vercel.app",
     ],
     credentials: true,
   })
